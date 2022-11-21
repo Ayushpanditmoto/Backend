935eeae1937734f668754f3ec8739b56c00364ac
Ayush Pandit <ayushpanditmoto@gmail.com>
2022-11-21 23:18:02 +0530

    added advanced filtering request query parameters added

--- controllers/bootcampsController.js
+++ controllers/bootcampsController.js
@@ -1,6 +1,7 @@
 const BootcampModel = require('../models/bootcampModel');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/asyncHandler');
+const geocoder = require('../utils/geocoders');
 
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await BootcampModel.findById(req.params.id);
@@ -45,8 +46,34 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await BootcampModel.find();
+  let query;
+  let queryStr = JSON.stringify(req.query);
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  query = BootcampModel.find(JSON.parse(queryStr));
+  const bootcamps = await query;
   res
     .status(200)
     .json({ success: true, count: bootcamps.length, data: bootcamps });
+});
+
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+  // Calc radius using radians
+  // Divide dist by radius of Earth
+  // Earth Radius = 3,963 mi / 6,378 km
+  const radius = distance / 3963;
+  const bootcamps = await BootcampModel.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
 });
