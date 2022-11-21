d6179f8e4d662e89280a69185f22e2d4a5902d51
Ayush Pandit <ayushpanditmoto@gmail.com>
2022-11-21 16:42:53 +0530

    added bootcamp routes and controller

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 //Load env variables
 require('dotenv').config({ path: './config.env' });
-
 const port = process.env.PORT || 3000;
-
 // console.log(typeof process.env.PORT);
+
+//Load routes
+const bootcamps = require('./routes/bootcampsRoutes');
+app.use('/api/v1/bootcamps', bootcamps);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
