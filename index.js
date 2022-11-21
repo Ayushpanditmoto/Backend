c2b645b35e0f8a6613b67648b36023d6dd218725
Ayush Pandit <ayushpanditmoto@gmail.com>
2022-11-21 16:54:42 +0530

    added morgan logger middleware

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 //Load env variables
 require('dotenv').config({ path: './config.env' });
-const port = process.env.PORT || 3000;
-// console.log(typeof process.env.PORT);
+const port = process.env.PORT || 3000; // console.log(typeof process.env.PORT);
+const morgan = require('morgan');
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// const logger = require('./middleware/logger');
+// app.use(logger);
 
 //Load routes
 const bootcamps = require('./routes/bootcampsRoutes');
